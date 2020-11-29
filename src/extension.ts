b9bb799ddadc3ff2e5f5f6d7bea9a21815ca6743
yanai101 <yanai101@gmail.com>
2020-11-29 18:21:43 +0200

    improve the logic

--- src/extension.ts
+++ src/extension.ts
@@ -1,90 +1,65 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
+const directoriesToIgnore = ["bower_components", "node_modules", "www", "platforms", "dist", ".git", ".idea", "build", "server"];
 
+const cssVars = new Map();
+let contextCopy: any;
 
-const directoriesToIgnore = [
-	"bower_components",
-	"node_modules",
-	"www",
-	"platforms",
-	"dist",
-	".git",
-	".idea",
-	"build",
-	"server"
-  ];
+async function getAllVariable(urlPath: string): Promise<any> {
+  let reader: any = null;
+  const pathDir = path.join(urlPath);
+  const currentDirectory = fs.readdirSync(pathDir, { withFileTypes: true });
 
-let cssVars: Array<{cssVar: string, val: string}> = []
-
-async function getAllVariable(urlPath:string): Promise<Array<{cssVar: string, val: string}>>{	
-	let reader:any = null;
-	let done = false;
-	const pathDir = await path.join(urlPath)
-	const currentDirectory = fs.readdirSync(pathDir, { withFileTypes: true });
-		while (currentDirectory.length > 0) {
-				const item:any = currentDirectory.pop();
-				if(item.isDirectory() && !directoriesToIgnore.includes(item.name)){
-					getAllVariable(path.join(pathDir, item.name));
-								
-				}
-				if(item.name.includes('css') || item.name.includes('scss')){
-					reader = fs.createReadStream(path.join(pathDir, item.name)); 
-					reader.on('data', async function (chunk:string) { 
-						await getCssVarFromChunk(chunk.toString()); 
-					});
-				}
-		}
-
-		
-		return new Promise((resolve, reject) =>{
-			reader && reader.on('end', () => { 
-				if(currentDirectory.length === 0){
-					resolve(cssVars)
-				}
-			}); 
-		})
- 
-	
+  currentDirectory.forEach(async (item) => {
+    if (item.isDirectory() && !directoriesToIgnore.includes(item.name)) {
+      getAllVariable(path.join(pathDir, item.name));
+    }
+    if (item.name.includes("css") || item.name.includes("scss")) {
+      reader = fs.createReadStream(path.join(pathDir, item.name));
+      reader.on("data", (chunk: string) => {
+        updateCssVarFromChunk(chunk.toString());
+      });
+    }
+  });
 }
 
-function getCssVarFromChunk(chunk:string){
-	const lines = chunk.split(/\r?\n/);
-	lines.forEach(line => {
-		const lineTrim = line.trim();
-	     if(lineTrim.length && lineTrim.startsWith('--')){
-			 const [cssVar, val]= lineTrim.split(":")
-			cssVars.push({cssVar, val})
-		 }
-	})
-	return ;
+function updateCssVarFromChunk(chunk: string) {
+  const cssVarsItems: vscode.CompletionItem[] = [];
+  const lines = chunk.split(/\r?\n/);
+  lines.forEach((line) => {
+    const lineTrim = line.trim();
+    if (lineTrim.length && lineTrim.startsWith("--")) {
+      const [cssVar, val] = lineTrim.split(":");
+      if (val && !cssVars.has(cssVar)) {
+        cssVarsItems.push(new vscode.CompletionItem(cssVar));
+        cssVars.set(cssVar, val);
+      }
+    }
+  });
+  const auto = vscode.languages.registerCompletionItemProvider(["css", "scss"], {
+    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+      return cssVarsItems;
+    },
+  });
+  contextCopy.subscriptions.push(auto);
+  return true;
 }
-
-
 
 export function activate(context: vscode.ExtensionContext) {
-	
-	const run = async() => {
-    console.log('YANAI',vscode.workspace.rootPath)
-    const data: any = await getAllVariable(vscode.workspace.rootPath as string);
+  contextCopy = context;
 
-		const cssVarsItems: vscode.CompletionItem[] = data.map((item:{cssVar: string}) => new vscode.CompletionItem(item.cssVar))
-		const auto = vscode.languages.registerCompletionItemProvider(['css','scss'], {
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
-			return cssVarsItems
-		}
-		})
-	
-		context.subscriptions.push(auto);
+  const run = async () => {
+    if (vscode.workspace.rootPath) {
+      const doneScan = await getAllVariable(vscode.workspace.rootPath as string);
+    }
+  };
 
-	}
-
-	run();
+  run();
 }
-
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
